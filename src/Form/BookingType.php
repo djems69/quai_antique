90393bbcf6f57237508b7580d7535c1b74873855
djems69 <djems69140@gmail.com>
2023-01-29 23:02:02 +0100

    add comment

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -19,18 +19,22 @@ class BookingType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        // Récupère l'email de l'utilisateur connecté à partir du service Security.
         $email = $this->security->getUser()->getEmail();
         $builder
+            // Ajoute un champ caché pour l'email avec la valeur déterminée ci-dessus.
             ->add('email', HiddenType::class, [
                 'label'=>'Mail',
                 'data'=>$email
                 ])
+            // Ajoute un champ de type DateType pour la date de la réservation.
                 ->add('day', DateType::class, [
                     'label'=>'Jour',
                     'widget' => 'single_text',
                     'format' => 'yyyy-MM-dd',
                     'model_timezone' => 'Europe/Paris',
                 ])
+            // Ajoute un champ de type TimeType pour l'heure de la réservation.
                 ->add('hour', TimeType::class, [
                     'label'=>'heure',
                     'input' => 'datetime',
@@ -38,7 +42,7 @@ class BookingType extends AbstractType
                     'hours' => [11, 12, 13, 14, 15, 18, 19, 20, 21, 22],
                     'minutes' => [00, 15, 30, 45],
                     ])
-
+            // Ajoute un champ de type TextType pour les allergies ou intolérances alimentaires.
             ->add('allergy', TextType::class, [
                 'label' => 'Allergies',
                 'required' => false,
@@ -46,22 +50,26 @@ class BookingType extends AbstractType
                     'placeholder' => 'Veuillez indiquer si vous avez des allergies ou des intolérances alimentaires.'
                ]
                 ])
+            // Ajout d'un champ pour le nombre de personnes dans la réservation
             ->add('seats', IntegerType::class, [
                 'label' => 'Nombre de personnes',
                 'attr' => ['min' => 1, 'max' => 10]
                 ])
+            // Ajout d'un bouton pour soumettre la réservation
             ->add('save', SubmitType::class, ['label' => 'Réserver'])
         ;
     }
 
+    // Déclaration de la variable de sécurité
     private $security;
 
+    // Constructeur qui prend en entrée un objet de sécurité
     public function __construct(Security $security)
     {
         $this->security = $security;
     }
 
-
+    // Configuration des options par défaut pour la classe de réservation
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
